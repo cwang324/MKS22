b7147714af7a451a45198302b46039309d88e61b
Carol Wang <cwang3@stuy.edu>
2016-02-22 12:27:47 -0500

    file stuff

---
+++ 04USACO/Bronze.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class Bronze {
+
+    public static void main (String[] args){
+
+	File inFile = new File( "makelake.txt" );
+
+	Scanner sc = new Scanner(inFile);
+	while (sc.hasNextLine()) {
+
+	    String line = sc.nextLine();
+	    
+	}
+
+	sc.close();
+	
+    }
+
+
+}
