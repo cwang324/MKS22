f82aff2478073dd3d13b41a22235b932f402f312
Carol Wang <cwang3@stuy.edu>
2016-03-03 01:32:50 -0500

    tried solving again

--- 04USACO/Silver.java
+++ 04USACO/Silver.java
@@ -4,34 +4,38 @@ import java.util.Scanner;
 public class Silver {
 	
 	private int[][] pasture;
-	private int R1, C1, endx, endy, movingTime;
+	private int R1, C1, endx, endy, movingTime, numRows, numCols,solution;
 	private boolean debug = true;
+	private Scanner sc;
 
 	
-	public Silver(String filename){
+	public Silver(){
 		
 		try{
-			File inFile = new File(filename);
-			Scanner sc = new Scanner(inFile);
-		
+			File inFile = new File("ctravel.in");
+			sc = new Scanner(inFile);
+		}catch(FileNotFoundException e){
+			System.out.println("File not found.");
+			}
 			
 				
-			int numRows = Integer.parseInt(sc.next());
-			int numCols = Integer.parseInt(sc.next());
+			numRows = Integer.parseInt(sc.next());
+			numCols = Integer.parseInt(sc.next());
 			movingTime = Integer.parseInt(sc.next());
 			
 			pasture = new int[numRows][numCols];
 			
 			String input="";
-			while (numRows>=0){
+			int rows = numRows;
+			while (rows>=0){
 		    	input+=sc.nextLine();
-		    	numRows--;;
+		    	rows--;;
 			}
 				
 			
 			int i = 0;
-		    for (int row=0; row<pasture.length; row++){
-		    	for (int col=0; col<pasture[0].length; col++){
+		    for (int row=0; row<numRows; row++){
+		    	for (int col=0; col<numCols; col++){
 		    		char cur = input.charAt(i);
 		    		
 		    		if (cur=='.'){
@@ -48,22 +52,22 @@ public class Silver {
 		    endx = Integer.parseInt(sc.next())-1;
 		    endy = Integer.parseInt(sc.next())-1;
 		    
+		    pasture[R1][C1]=1;
+		    
 			if (debug){
 				System.out.println(numRows + " " + numCols);
 				System.out.println(input + " " + input.length());
 				System.out.println(R1 + " " + C1 + " " + endx + " " + endy);
 			}
 			
-		}catch(FileNotFoundException e){
-			System.out.println("File not found.");
-			}
+		
 	}
 		
 		 public String toString(){
    			 String retString = "";
-     		   	for (int rows=0; rows < pasture.length; rows++){
+     		   	for (int rows=0; rows < numRows; rows++){
 			    		
-			  		for (int cols=0; cols < pasture[0].length; cols++){
+			  		for (int cols=0; cols < numCols; cols++){
 			    		retString+=pasture[rows][cols]+"\t";
 			    			
 		    		}
@@ -81,15 +85,32 @@ public class Silver {
 		    		
 			  		for (int cols=0; cols < pasture[0].length; cols++){
 			  				
-			  				if (pasture[rows][cols]==0){
-			  					newPasture[rows][cols]= add(rows-1,cols) + add(rows, cols-1) 
-			    									+ add(rows+1,cols) + add(rows, cols+1);
-			  					
-			  				}else if (pasture[rows][cols]>0){
-				  				newPasture[rows][cols]=0;
-			  				}else{
-			  					newPasture[rows][cols]=-1;
-			  				}
+			  				if (pasture[rows][cols]>=0){
+			  					int sum = 0;
+			  					if(cols - 1 >= 0 && pasture[rows][cols - 1] > 0){
+									sum = sum + pasture[rows][cols - 1];
+								}
+								if(cols + 1 < cols && pasture[rows][cols + 1] > 0){
+									sum = sum + pasture[rows][cols + 1];
+								}
+								if(rows - 1 >= 0 && pasture[rows - 1][cols] > 0){
+									sum = sum + pasture[rows - 1][cols];
+								}
+								if(rows + 1 < rows && pasture[rows + 1][cols] > 0){
+									sum = sum + pasture[rows + 1][cols];
+								}
+								newPasture[rows][cols] = sum;
+							}else{
+								newPasture[rows][cols] = -1;
+							}
+//			  					newPasture[rows][cols]= add(rows-1,cols) + add(rows, cols-1) 
+//			    									+ add(rows+1,cols) + add(rows, cols+1);
+//			  					
+////			  				}else if (pasture[rows][cols]>0){
+////				  				newPasture[rows][cols]=0;
+//			  				}else{
+//			  					newPasture[rows][cols]=-1;
+//			  				}
 			  				
 			  				//System.out.println(newPasture[rows][cols]);
 	  				}				  				
@@ -114,46 +135,68 @@ public class Silver {
 			 return pasture[row][col];
 		 }
 		 
-		private void start(){
-			
-			if (!(R1<1 )){
-				if (pasture[R1-1][C1]!=-1){
-					pasture[R1-1][C1]=1;
-				}
-			}
-			
-			if (!(C1<1)){
-				if( pasture[R1][C1-1]!=-1 ){
-					pasture[R1][C1-1]=1;
-				}
-			}
-			
-			if (!(R1>pasture.length-1)){
-				if (pasture[R1+1][C1]!=-1){
-					pasture[R1+1][C1]=1;
-				}
-			}
-			
-			if (!(C1>pasture[R1].length-1)){
-				if (pasture[R1][C1+1]!=-1){
-					pasture[R1][C1+1]=1;
-				}
-			}
-			 
-		}
+
 		 
-		private int solve(int T){
-			if(T==0){
-				return pasture[endx][endy];
-			}else{
-				changeState();
-				return solve(T-1);
-			}
+		private boolean solve(){
+			while(movingTime>0){
+				
+				 int[][] newPasture = new int[numRows][numCols]; 
+				 
+				 for (int rows=0; rows < numRows; rows++){
+			    		
+				  		for (int cols=0; cols < numCols; cols++){
+				  				
+				  				if (pasture[rows][cols]>=0){
+				  					int sum = 0;
+				  					
+				  					// add from the left
+				  					if(cols - 1 >= 0 && pasture[rows][cols - 1] > 0){
+										sum += pasture[rows][cols - 1];
+									}
+				  					
+				  					// add from the right
+									if(cols + 1 < cols && pasture[rows][cols + 1] > 0){
+										sum +=  pasture[rows][cols + 1];
+									}
+									
+									// add from the top
+									if(rows - 1 >= 0 && pasture[rows - 1][cols] > 0){
+										sum += pasture[rows - 1][cols];
+									}
+									
+									// add from the bottom
+									if(rows + 1 < rows && pasture[rows + 1][cols] > 0){
+										sum +=  pasture[rows + 1][cols];
+									}
+									newPasture[rows][cols] = sum;
+								}else{
+									newPasture[rows][cols] = -1;
+								}
+//				  					newPasture[rows][cols]= add(rows-1,cols) + add(rows, cols-1) 
+//				    									+ add(rows+1,cols) + add(rows, cols+1);
+//				  					
+////				  				}else if (pasture[rows][cols]>0){
+////					  				newPasture[rows][cols]=0;
+//				  				}else{
+//				  					newPasture[rows][cols]=-1;
+//				  				}
+				  				
+				  				//System.out.println(newPasture[rows][cols]);
+		  				}				  				
+				   }
+				 
+				 pasture = newPasture;
+				 movingTime = movingTime-1;
+				 System.out.println(this);
+			}	
+			solution = pasture[endx][endy];
+			System.out.println(solution);
+			return true;
 		}
 		 
 		public static void main(String[] args){
 		
-			Silver s = new Silver("ctravel.in");
+			Silver s = new Silver();
 			
 			
 			
@@ -161,29 +204,29 @@ public class Silver {
 			if (s.debug){
 				
 				System.out.println(s);
-				s.start();
+				//s.start();
 				
-				System.out.println(s);
-				s.changeState();
-				
-				System.out.println(s);
-				s.changeState();
-				
-				System.out.println(s);
-				s.changeState();
-				
-				System.out.println(s);
-				s.changeState();
-				
-				System.out.println(s);
-				s.changeState();
-				
-				System.out.println(s);
-				s.changeState();
-				
-				System.out.println(s);
-				s.changeState();
-				//System.out.println(s.solve(s.movingTime));
+//				System.out.println(s);
+//				s.changeState();
+//				
+//				System.out.println(s);
+//				s.changeState();
+//				
+//				System.out.println(s);
+//				s.changeState();
+//				
+//				System.out.println(s);
+//				s.changeState();
+//				
+//				System.out.println(s);
+//				s.changeState();
+//				
+//				System.out.println(s);
+//				s.changeState();
+//				
+//				System.out.println(s);
+//				s.changeState();
+				s.solve();
 				
 				
 			}
