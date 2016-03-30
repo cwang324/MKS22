95625b237caba35b10b6c4c5bf38b3f100e33d45
Carol Wang <cwang3@stuy.edu>
2016-03-29 20:32:44 -0400

    Added files via upload

--- 09StackQueue/MyStack.java
+++ 09StackQueue/MyStack.java
@@ -3,25 +3,24 @@ import java.util.NoSuchElementException;
 
 public class MyStack<T> {
 
-    MyLinkedList<T> L = new MyLinkedList();
-
+	MyLinkedList<T> L;
 	
 	/**
      * Adds the given item to the top of the stack.
      */
-    public void push(T item){
-    	L.add(item);
+    void push(T item){
+    	L.add(0, item);
     };
 
     /**
      * Removes the top item from the stack and returns it.
      * @exception java.util.NoSuchElementException if the queue is empty.
      */
-    public T pop(){
+    T pop(){
     	if (isEmpty()){
     		throw new NoSuchElementException();
     	}
-    	return L.remove(size()-1);
+    	return L.remove(0);
     };
 
     /**
@@ -32,46 +31,22 @@ public class MyStack<T> {
     	if (isEmpty()){
     		throw new NoSuchElementException();
     	}
-    	return L.get(size()-1);
+    	return L.get(0);
     };
 
     /**
      * Returns the number of items currently in the stack.
      */
-    public int size(){
+    int size(){
     	return L.size();
     };
 
     /**
      * Returns whether the stack is empty or not.
      */
-    public boolean isEmpty(){
+    boolean isEmpty(){
     	return L.size()==0;
     };
-
-    public static boolean isMatching(String s){
-	MyStack<Character> input = new MyStack<Character>();
-	for (int i=0; i < s.length(); i++){
-	    input.push(s.charAt(i));
-	}
-	
-
-	System.out.println(input.L);
-	return true;
-    }
-
-   
-
-
-
-    public static void main(String[] args){
-	String input="";
-	if(args.length > 0){
-	    input = args[0];
-	}
-
-	System.out.println(isMatching(input));
-    }
 }
 	
 	
