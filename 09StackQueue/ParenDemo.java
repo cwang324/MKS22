292e40428afdf95f854de2a9299e8e270629f211
Carol Wang <cwang3@stuy.edu>
2016-03-29 22:10:14 -0400

    Added files via upload

--- 09StackQueue/ParenDemo.java
+++ 09StackQueue/ParenDemo.java
@@ -69,68 +69,7 @@ public class ParenDemo {
 			
 	}
 	
-//public static boolean isMatching(String s){
-//		
-//		
-//		// putting input into Linked List
-//		MyStack<Character> stack = new MyStack<Character>();
-//		
-//		char c;
-//		for (int i=0; i<s.length(); i++){
-//			c = s.charAt(i);
-//			
-//			if (isValidOpen(c)){
-//				
-//				stack.push(c);
-//				
-//			}else if (c==')'){
-//					
-//				if(stack.isEmpty()){
-//					return false;
-//				}
-//				if (stack.pop() != '('){
-//					return false;
-//				}
-//					
-//					
-//				}else if (c=='}'){
-//					
-//					if(stack.isEmpty()){
-//						return false;
-//					}
-//					if (stack.pop() != '{'){
-//						return false;
-//					}
-//				}else if (c==']'){
-//					
-//					if(stack.isEmpty()){
-//						return false;
-//					}
-//					if (stack.pop() != '['){
-//						return false;
-//					}
-//				}
-//				
-//				
-//			}	
-//		}
-//				
-//			return stack.isEmpty();
-//			
-//	}
-	
-//	public static boolean isMatchingH(MyStack s, char c){
-//		if (s.isEmpty()){
-//			return false;
-//		}
-//		else if (s.peek().equals(c)){
-//			s.pop();
-//			return true;
-//		}
-//		else{
-//			return false;
-//		}
-//	}
+
 			
 	
 	 public static void main(String[]args){
