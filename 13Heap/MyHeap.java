deafcdc3262126a4ebefd92db8ca42dacaa1ce6a
Carol Wang <cwang3@stuy.edu>
2016-05-05 23:58:17 -0400

    Added files via upload

--- 13Heap/MyHeap.java
+++ 13Heap/MyHeap.java
@@ -27,13 +27,41 @@ public class MyHeap<T extends Comparable<T>>
 
    private void pushUp(int k){
        T temp = data[k];
-
-   private void heapify(){
+       data[k] = data[k/2];
+       data[k/2] = temp;
+   }
        
 
+   private void heapify(){
+       int index = 1;
+       while (index >= 1 && index <= size/2){
+    	   if (data[index].compareTo(data[index*2])<0 || data[index].compareTo(data[index*2+1]) < 0 ){
+    		   if ( data[index].compareTo(data[index*2]) < 0 ){
+    			   pushDown(index);
+    			   
+    		   }   		   
+    	   }
+    	   index++;
+       }
    }
-   // public T delete()
-   // public void add(T x)
+   
+   public T delete(){
+	   T removed = data[1];
+	   data[1] = data[size];
+	   heapify();
+	   size--;
+	   return removed;	   	   
+   }
+   
+   public void add(T x){
+	   if (size==0){
+		   data[1] = x;
+	   }else{
+		   	data[size+1]=x;
+		   	heapify();
+	   }
+	   size++;
+   }
    // private void doubleSize()
    // public String toString()
 
