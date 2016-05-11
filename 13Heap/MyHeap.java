177982107144766f1872829c85c493a1847d3979
Carol Wang <cwang3@stuy.edu>
2016-05-10 23:50:39 -0400

    Add files via upload

--- 13Heap/MyHeap.java
+++ 13Heap/MyHeap.java
@@ -6,6 +6,7 @@ public class MyHeap<T extends Comparable<T>>
 {
    private int size;
    private T[] data;
+   private boolean isMax;
 
    public MyHeap(){
        size = 0;
@@ -18,6 +19,16 @@ public class MyHeap<T extends Comparable<T>>
        heapify();
    }
 
+   public MyHeap(boolean isMax){
+	   this();
+	   this.isMax = isMax;
+   }
+   
+   public MyHeap(T[] array, boolean isMax){
+	   this(array);
+	   this.isMax = isMax;
+	   
+   }
    
    private void swap(int k1, int k2) {
        T tmp = data[k1];
@@ -27,7 +38,14 @@ public class MyHeap<T extends Comparable<T>>
    
    
    private void pushDown(int k){
-
+	   if (isMax){
+		   pushDownMax(k);
+	   }else{
+		   pushDownMin(k);
+	   }  
+   }
+   
+   private void pushDownMax(int k){
 	   while (k*2+1 <= size){
 		   int smaller = k*2;
 	   
@@ -45,14 +63,48 @@ public class MyHeap<T extends Comparable<T>>
 		   
 		   k = smaller;
 	   }
-	   
    }
 
+   private void pushDownMin(int k){
+	   while (k*2+1 <= size){
+		   int larger = k*2;
+	   
+	   
+		   if ( ((k*2+1) <= size) &&
+			   data[2*k].compareTo(data[2*k+1]) < 0){
+			   		larger = 2*k+1;
+		   }
+		   
+		   if (data[k].compareTo(data[larger]) < 0){
+			   swap(k, larger);
+		   }else{
+			   break;
+		   }
+		   
+		   k = larger;
+	   }
+   }
+   
    private void pushUp(int k){
-       while (k > 1 && data[k/2].compareTo(data[k]) > 0){
+	   if (isMax){
+		   pushUpMax(k);
+	   }else{
+		   pushUpMin(k);
+	   }  
+   }
+   
+   private void pushUpMax(int k){
+	   while (k > 1 && data[k/2].compareTo(data[k]) > 0){
     	   swap(k, k/2);
     	   k = k/2;
     	
+       }
+   }
+   
+   private void pushUpMin(int k){
+	   while (k > 1 && data[k/2].compareTo(data[k]) < 0){
+    	   swap(k, k/2);
+    	   k = k/2;
        }
    }
        
@@ -100,8 +152,7 @@ public class MyHeap<T extends Comparable<T>>
 	   return Arrays.toString(data);
    }
 
-   // //do this last
-   // public MyHeap(boolean isMax)
-   // public MyHeap(T[] array, boolean isMax)
+
+  
 
 }
